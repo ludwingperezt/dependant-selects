bcce1af1814b078eaa198c6d6c05b7a5d3517308
Ludwing Pérez <ludwing@ludwing-idei>
2019-03-13 15:54:54 -0600

    Basic code for the dependant selects snippet.

---
+++ dependant_selects.js
@@ -0,0 +1,53 @@
+function initDependentSelects(parentSelector, 
+    childSelector, 
+    parentData,
+    childData,
+    childPlaceholder) {
+
+    var parentElement = $(parentSelector);
+    var childElement = $(childSelector);
+    var parentSelectedValue = parentElement.val();
+    var childSelectedValue = childElement.val();
+
+    childElement.empty();
+    parentElement.select2({
+        data: parentData
+    });
+    childElement.select2({
+        placeholder: childPlaceholder
+    });
+    parentElement.on('select2:select', function(e) {
+        var parentKey = e.params.data.id;
+        filterChild(childSelector, parentKey, childData);
+    });
+
+    // Pre-select child value if parent has a selected value
+    if (parentSelectedValue) {
+        filterChild(childSelector, parentSelectedValue, childData);
+
+        if (childSelectedValue) {
+            childElement.val(childSelectedValue).trigger('change.select2');
+        }
+    }
+}
+
+function searchChildElements(parentKey, list) {
+    var found_elements = list.filter(function(val){
+        return val.parentID === parseInt(parentKey);
+    });
+    return found_elements;
+}
+
+function populateChild(childSelector, data) {
+    $(childSelector).empty();
+    $(childSelector).select2({
+        data: data
+    })
+    ;
+}
+
+function filterChild(childSelector, parentKey, list, placeholder='--') {
+    var filtered = searchChildElements(parentKey, list);
+    filtered.splice(0,0,{id:'', text: placeholder});
+    populateChild(childSelector, filtered);
+}
