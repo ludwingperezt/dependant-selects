d408f95b53f6eec1acf05db9ece9f9a2f0355759
Ludwing Pérez <ludwing@ludwing-idei>
2019-03-15 09:44:29 -0600

    Added function initSelects that allows init the selects with select2 configs as parameters.

--- dependant_selects.js
+++ dependant_selects.js
@@ -31,6 +31,35 @@ function initDependentSelects(parentSelector,
     }
 }
 
+function initSelects(parentSelector, 
+    childSelector, 
+    parentConfig,
+    childConfig) {
+
+    var parentElement = $(parentSelector);
+    var childElement = $(childSelector);
+    var parentSelectedValue = parentElement.val();
+    var childSelectedValue = childElement.val();
+
+    childElement.empty();
+    parentElement.select2(parentConfig);
+    childElement.select2(childConfig);
+
+    parentElement.on('select2:select', function(e) {
+        var parentKey = e.params.data.id;
+        filterChild(childSelector, parentKey, childConfig.data);
+    });
+
+    // Pre-select child value if parent has a selected value
+    if (parentSelectedValue) {
+        filterChild(childSelector, parentSelectedValue, childConfig.data);
+
+        if (childSelectedValue) {
+            childElement.val(childSelectedValue).trigger('change.select2');
+        }
+    }
+}
+
 function searchChildElements(parentKey, list) {
     var found_elements = list.filter(function(val){
         return val.parentID === parseInt(parentKey);
